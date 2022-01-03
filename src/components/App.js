eff73e75eb5b07737a06dde0f94bc938b3a024ee
Kyler Best <kyler.b@live.com>
2022-01-03 14:59:49 -0600

    commit tree 394d18be42fdbbe5860f1126fae9ab068eba2b69

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
+import React, {useEffect, useState} from "react";
 // create your App component here
+
+function App(){
+    const [content, setContent] = useState(<p>Loading...</p>)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(r => r.json())
+            .then(json => setContent(<img src={json.message} alt="A Random Dog" />))
+    }, [])
+
+    return (
+        content
+    )
+}
+
+export default App
